c8e548c73858bbf85322f519c6d84b689319ef04
Matheus Varistelo <105511894+Apollon-S@users.noreply.github.com>
2024-09-30 16:34:56 -0300

    Atualizando login e register // Renovação estética

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let dotCount = 1;
 const dotAnimation = setInterval(() => {
     dotCount = (dotCount % 3) + 1; // Alterna entre 1 a 3 pontos
     dots.textContent = '.'.repeat(dotCount);
-}, 500); // Ajuste o intervalo conforme desejado
+}, 300); // Ajuste o intervalo conforme desejado
 
 // Carregamento
 const loadingInterval = setInterval(() => {
@@ -19,9 +19,22 @@ const loadingInterval = setInterval(() => {
         clearInterval(dotAnimation); // Para a animação dos pontos
         splash.style.display = 'none';
         welcome.style.display = 'block';
+        animateDivs();
     } else {
         progress++;
         progressBar.style.width = progress + '%';
         percentageText.textContent = progress + '%';
     }
-}, 25); // Ajuste a velocidade do carregamento se necessário
+}, 16); // Ajuste a velocidade do carregamento se necessário
+
+function animateDivs() {
+    // Pega o ID das divs que ocorrerá a animação
+    const div1 = document.getElementById("splash");
+    const div2 = document.getElementById("welcome");
+    const div3 = document.getElementById("login");
+    const div4 = document.getElementById("register")
+  
+    // Adiciona a classe de animação para as divs
+    div1.classList.add("animate-out");
+    div2.classList.add("animate-in");
+  }

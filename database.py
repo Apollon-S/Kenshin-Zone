f5d3b979cdd71e1a83dcb0156831ce29094e498a
Apollon-S <matheusvaristelom@gmail.com>
2024-11-21 14:21:03 -0300

    Update db

--- database.py
+++ database.py
@@ -4,13 +4,13 @@ def initdb():
     link = sqlite3.connect("Kenshin_Zone.db")
     cursor = link.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS users
-    (ID INTEGER PRIMARY KEY, email TEXT not NULL, user TEXT not NULL, password TEXT not NULL, gender TEXT not NULL, body_weight FLOAT not NULL, time INTEGER not NULL, XP INTEGER not NULL)''')
+    (IDuser INTEGER PRIMARY KEY, email TEXT not NULL, user TEXT not NULL, password TEXT not NULL, gender TEXT not NULL, body_weight FLOAT not NULL, time INTEGER not NULL, XP INTEGER not NULL)''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS workouts
-                   (IDworkout INTEGER PRIMARY KEY, name TEXT not NULL, IDUSER INTEGER not NULL)''')
+                   (IDworkout INTEGER PRIMARY KEY, name TEXT not NULL, IDUSER INTEGER not NULL, FOREIGN KEY (IDUSER) REFERENCES users (IDuser) )''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS exercises
                    (IDexercise INTEGER PRIMARY KEY, name TEXT not NULL, muscular_group TEXT not NULL)''')    
     cursor.execute('''CREATE TABLE IF NOT EXISTS progressions
-                (data, weight FLOAT not NULL, reps INTEGER not NULL, sets INTEGER not NULL)''')    
+                (date DATE, weight FLOAT not NULL, reps INTEGER not NULL, sets INTEGER not NULL)''')    
     
 
 
